2f8a25c1e634f8e212dff0bf9691d9c60cacde83
chienapc <trunkvtv123@gmail.com>
2025-01-11 23:24:18 +0700

    feat: create models and their relationship

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,22 +1,66 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize.config.js";
 
-export const User = sequelize.define("User", {
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
+export const User = sequelize.define(
+    "User",
+    {
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        google_id: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dob: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.ENUM("active", "deactive"),
+            defaultValue: "active",
         },
     },
-    age: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        },
+    {
+        tableName: "users",
     },
-});
+);
+
+User.associations = (models) => {
+    User.belongsToMany(models.Role, {
+        through: models.User_Role,
+        foreignKey: "user_id",
+        as: "roles",
+    });
+};
 
 export default (sequelize, DataTypes) => {
     return User;
