3e5a0abff75446be165cf66c3dc9540692495df3
chienapc <trunkvtv123@gmail.com>
2025-01-14 11:54:22 +0700

    feat: register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,10 @@
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/config.js";
+import sequelize from "../config/sequelize.config.js";
+import { Role } from "../models/role.model.js";
 import { User } from "../models/user.model.js";
+import { User_Role } from "../models/user_role.model.js";
 
 export const login = (req, res) => {
     // get email, password from user through request.body
@@ -41,4 +45,68 @@ export const login = (req, res) => {
                 error: "An error occured during login!",
             });
         });
+};
+
+// Function to hash password
+const hashPassword = (password) => {
+    const salt = bcrypt.genSaltSync(8); // Generate a unique salt for each password
+    return bcrypt.hashSync(password, salt);
+};
+
+export const register = async (req, res) => {
+    const transaction = await sequelize.transaction(); // bắt đầu 1 transaction
+    try {
+        const { email, password, phone, firstname, lastname, dob, gender, status } = req.body;
+
+        // Check if the user already exists
+        const isUserExist = await User.findOne({
+            where: { email: email },
+        });
+
+        if (!isUserExist) {
+            // Hash the password
+            const hashedPassword = hashPassword(password);
+
+            // Create the user
+            const user = await User.create({
+                email,
+                password: hashedPassword,
+                phone,
+                firstname,
+                lastname,
+                dob,
+                gender,
+                status,
+            });
+
+            // Assign default role
+            const role_default = await Role.findOne({
+                where: { role_name: "USER" },
+            });
+
+            if (role_default) {
+                await User_Role.create({
+                    user_id: user.user_id,
+                    role_id: role_default.role_id,
+                }); // Make sure `addRole` is correctly set in associations
+            }
+
+            await transaction.commit();
+
+            res.status(200).json({
+                code: 200,
+                message: "Register successfully!",
+            });
+        } else {
+            res.status(400).json({
+                code: 400,
+                message: "This email has already been used to register another account.",
+            });
+        }
+    } catch (error) {
+        await transaction.rollback();
+        res.status(500).json({
+            error: "An error occured during login!",
+        });
+    }
 };
