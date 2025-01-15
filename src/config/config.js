89e55569c04f0133462656f0f50c00b7f65cc906
Meocute5104 <nguyendangkhoa5104@gmail.com>
2025-01-15 15:44:12 +0700

    feat: add sequelize

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,10 @@ dotenv.config();
 export const JWT_SECRET = process.env.JWT_SECRET || "jwtsecret";
 
 // DB config
-export const DATABASE = "user";
-export const USERNAME = "root";
-export const PASSWORD = process.env.PASSWORD || "spring";
-export const DIALECT = process.env.DIALECT || "mysql";
-export const DBHOST = process.env.DIALECT || "localhost";
+// export const DATABASE = "testDB";
+// export const USERNAME = "testDB_owner";
+// export const PASSWORD = "IEw2dbUDihn9";
+// export const DIALECT = "postgres";
+// export const DBHOST = "ep-soft-base-a58sfsqj.us-east-2.aws.neon.tech";
+
+export const DB = "postgresql://postgres:12345@localhost:5432/jspu";
